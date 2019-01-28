b71aa708362807b9631ec5a3108a1aa9a2f6968a
Ibrahim Bouriga <ibrahim.bouriga@gmail.com>
2019-01-28 21:54:52 +0100

    Network Implementation

---
+++ ImageClassificationSystem/src/Network.cpp
@@ -0,0 +1,63 @@
+/*
+ * Network.cpp
+ *
+ *  Created on: Jan 28, 2019
+ *      Author: ibrahim
+ */
+
+#include "Network.h"
+
+Network::Network() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Network::~Network() {
+	// TODO Auto-generated destructor stub
+}
+
+void Network::run_network(Image input, Network net) {
+	int i;
+	double *input_d = 0;
+	for (i = 0; i < net.n; ++i) {
+		if (net.types[i] == CONVOLUTIONAL) {
+			Convolutional_layer layer = *(Convolutional_layer *)net.layers[i];
+			layer.run_convolutional_layer(input);
+			input = layer.output; // output is the input of the next layer
+			input_d = layer.output.data; // output data is the input data of the next layer
+		}
+		else if (net.types[i] ==  CONNECTED) {
+			Connected_layer layer = *(Connected_layer *)net.layers[i];
+			layer.run_connected_layer(input_d);
+			// THERE IS NO OUTPUT IMAGE DELIVERED BY CONNECTED LAYER
+			input_d = layer.output;
+		}
+		else if (net.types[i] == MAXPOOL) {
+			Maxpool_layer layer = *(Connected_layer *)net.types[i];
+			layer.run_maxpool_layer(input);
+			input = layer.output; // output is the input of the next layer
+			input_d = layer.output.data; // output data is the input data of the next layer
+		}
+	}
+}
+
+Image Network::get_network_image(Network net) {
+
+	int i;
+	for (i = net.n - 1; i >= 0; --i) {
+		if (net.types[i] == CONVOLUTIONAL) {
+			Convolutional_layer layer = *(Convolutional_layer *)net.layers[i];
+			return layer.output;
+		}
+		else if (net.types[i] == CONNECTED) {
+			Connected_layer layer = *(Connected_layer *)net.layers[i];
+			return layer.output;
+		}
+		else if (net.types[i] == MAXPOOL) {
+			Maxpool_layer layer = *(Maxpool_layer *)net.layers[i];
+			return layer.output;
+		}
+	}
+	return Image(1,1,1);
+}
+
---
+++ ImageClassificationSystem/src/Network.h
@@ -0,0 +1,35 @@
+/*
+ * Network.h
+ *
+ *  Created on: Jan 28, 2019
+ *      Author: ibrahim
+ */
+
+#ifndef NETWORK_H_
+#define NETWORK_H_
+
+#include "Image.h"
+#include "Convolutional_layer.h"
+#include "Connected_layer.h"
+#include "Maxpool_layer.h"
+
+class Network {
+private:
+	typedef enum {
+		CONVOLUTIONAL,
+		CONNECTED,
+		MAXPOOL
+	} LAYER_TYPE;
+
+	int n; // number of networks available
+	void **layers; // layers will be defined through type casting
+	LAYER_TYPE *types;
+public:
+	Network();
+	virtual ~Network();
+
+	void run_network(Image input, Network net);
+	Image get_network_image(Network net);
+};
+
+#endif /* NETWORK_H_ */
