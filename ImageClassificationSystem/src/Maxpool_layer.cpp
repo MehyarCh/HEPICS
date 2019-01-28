1317889be568f406af027bc37a138b17ad4b1a27
Ibrahim Bouriga <ibrahim.bouriga@gmail.com>
2019-01-28 21:53:44 +0100

    MaxPool layer Implementation

---
+++ ImageClassificationSystem/src/Maxpool_layer.cpp
@@ -0,0 +1,32 @@
+/*
+ * Maxpool_layer.cpp
+ *
+ *  Created on: Jan 28, 2019
+ *      Author: ibrahim
+ */
+
+#include "Maxpool_layer.h"
+#include <float.h>
+
+Maxpool_layer::Maxpool_layer(int h, int w, int c, int stride) {
+	this->stride = stride;
+	this->output = Image((h - 1) / stride + 1, (w - 1) / stride + 1, c);
+}
+
+Maxpool_layer::~Maxpool_layer() {
+	// TODO Auto-generated destructor stub
+}
+
+void Maxpool_layer::run_maxpool_layer(Image input) {
+    int i,j,k;
+    for(i = 0; i < this->output.height * this->output.width *this->output.channels; ++i) this->output.data[i] = -DBL_MAX;
+    for(i = 0; i < input.height; ++i){
+        for(j = 0; j < input.width; ++j){
+            for(k = 0; k < input.channels; ++k){
+                double val = input.get_pixel(i, j, k);
+                double cur = this->output.get_pixel(i / this->stride, j / this->stride, k);
+                if(val > cur) this->output.set_pixel(i / this->stride, j / this->stride, k, val);
+            }
+        }
+}
+}
---
+++ ImageClassificationSystem/src/Maxpool_layer.h
@@ -0,0 +1,23 @@
+/*
+ * Maxpool_layer.h
+ *
+ *  Created on: Jan 28, 2019
+ *      Author: ibrahim
+ */
+
+#ifndef MAXPOOL_LAYER_H_
+#define MAXPOOL_LAYER_H_
+
+#include "Image.h"
+
+class Maxpool_layer {
+private:
+	int stride;
+	Image output;
+public:
+	Maxpool_layer(int h, int w, int c, int stride);
+	virtual ~Maxpool_layer();
+	void run_maxpool_layer(Image input);
+};
+
+#endif /* MAXPOOL_LAYER_H_ */
