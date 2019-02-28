700b973872ac42d15fd21f2dea196f18edd2986e
LinjuanFan <43440419+ilonafan@users.noreply.github.com>
2019-02-28 02:42:37 -0800

    fix image as 4D

--- layers/Local_response_normalization_layer.cpp
+++ layers/Local_response_normalization_layer.cpp
@@ -1,5 +1,4 @@
-/*]\
- * '''''''''''''''''''''''''''''''
+/*
  * Local_response_normalization_layer.cpp
  *
  *  Created on: Feb 27, 2019
@@ -21,28 +20,31 @@ unique_ptr<Image> Local_response_normalization_layer::forward_layer(const Image 
 	auto output_width = input.width;
 	auto output_height = input.height;
 	auto output_channels = input.channels;
+	auto output_num = input.num;
 
-	auto output = make_unique<Image>(output_width, output_height, output_channels);
+	auto output = make_unique<Image>(output_width, output_height, output_channels, output_num);
 
     int local_size = 5;
     float alpha = 0.0001;
     float beta = 0.75;
 
-	for (int y = 0; y < output->height; y++) {
-		for (int x = 0; x < output->width; x++) {
-			for (int c = 0; c < output->channels; c++) {
-				float product = 0;
-				int from = fmax (c - local_size, 0);
-				int to = fmin (input.channels - 1, c + local_size);
-				for (int i = from; i <= to; i++) {
-					float value = input.at(x, y, i);
-					product += value * value;
-				}
-			    output->at(x, y, c) = input.at(x, y, c) / pow(alpha * product, beta);
-			}
-		}
-	}
-	return output;
+    for (int n = 0; n < output->num; n++) {
+	    for (int y = 0; y < output->height; y++) {
+		    for (int x = 0; x < output->width; x++) {
+			    for (int c = 0; c < output->channels; c++) {
+				    float product = 0;
+				    int from = fmax (c - local_size, 0);
+				    int to = fmin (input.channels - 1, c + local_size);
+				    for (int i = from; i <= to; i++) {
+					    float value = input.at(x, y, i, n);
+					    product += value * value;
+				    }
+			            output->at(x, y, c, n) = input.at(x, y, c, n) / pow(alpha * product, beta);
+			    }
+		     }
+	     }
+     }
+     return output
 }
 
 } // namespace hepics
